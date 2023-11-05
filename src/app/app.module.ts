1f56dce6a38b0dd5cdbd1ed64b3da4d7d90b0181
HotCheetos101 <120177674+HotCheetos101@users.noreply.github.com>
2023-11-05 17:45:44 +0800

    initialized log-in page and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
