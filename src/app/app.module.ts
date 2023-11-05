58c5674492415e48c5e0a73862e7eedebe0ea670
Archilece <Archilecejoleos@gmail.com>
2023-11-05 17:47:45 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderUserComponent } from './header-user/header-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderUserComponent
   ],
   imports: [
     BrowserModule,
