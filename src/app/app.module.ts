8ee017e4afc553fa96e90b162a8b17d42553f4aa
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-11-09 22:48:32 +0800

    ADDED register USER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { VendorMainPageComponent } from './vendor-main-page/vendor-main-page.com
 import { VendorSideBarComponent } from './vendor-side-bar/vendor-side-bar.component';
 import { VendorAddItemComponent } from './vendor-add-item/vendor-add-item.component';
 import { CurrencyCalcuVendorComponent } from './currency-calcu-vendor/currency-calcu-vendor.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { CurrencyCalcuVendorComponent } from './currency-calcu-vendor/currency-c
     CurrencyConverterComponent,
     VendorSideBarComponent,
     VendorAddItemComponent,
-    CurrencyCalcuVendorComponent
+    CurrencyCalcuVendorComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
