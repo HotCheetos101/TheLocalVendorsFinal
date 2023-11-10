d25d05089e31939580703fda1ddb83bd49cbdc47
Archilece <Archilecejoleos@gmail.com>
2023-11-10 21:28:08 +0800

    Routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginVendorComponent } from './login-vendor/login-vendor.component';
 import { VendorMainPageComponent } from './vendor-main-page/vendor-main-page.component';
-import { VendorAddItemComponent } from './vendor-add-item/vendor-add-item.component';       
+import { VendorAddItemComponent } from './vendor-add-item/vendor-add-item.component';
+import { LoginUserComponent } from './login-user/login-user.component'; 
+import { RegisterVendorComponent } from './register-vendor/register-vendor.component';      
 const routes: Routes = [
  {path: '', component: LoginPageComponent},
  {path: 'login-vendor', component: LoginVendorComponent},
  {path: 'main-vendor', component: VendorMainPageComponent},
  {path: 'add-item-vendor', component: VendorAddItemComponent},
+ {path: 'login-user', component: LoginUserComponent},
+ {path: 'vendor-registration', component: RegisterVendorComponent},
 ];
 
 @NgModule({
