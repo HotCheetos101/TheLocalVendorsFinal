819f581d43069db422b003f9c2822a84654ffc70
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2023-11-10 21:27:08 +0800

    added na

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LoginUserComponent } from './login-user/login-user.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -36,7 +36,8 @@ import { ForumTopicViewComponent } from './forum-topic-view/forum-topic-view.com
     VendorAddItemComponent,
     VendorItemPageComponent,
     CurrencyCalcuVendorComponent,
-    ForumTopicViewComponent
+    ForumTopicViewComponent,
+    LoginUserComponent
   ],
   imports: [
     BrowserModule,
